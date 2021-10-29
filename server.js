1bc18e99625a161bc32e82d1cfdf86467ac73191
skyyudha <dmsleroux@gmail.com>
2021-10-29 17:52:15 +0700

    jwt verify success
    masih jws dengan secret hs256

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 // const jwt = require("jsonwebtoken") //get library JWT
 const { jwtVerify } = require('jose')
 app.use(express.json()) //pasang JWT ke Apps
-const algorithm = 'RS256'
-const pkcs8 = fs.readFileSync('./jwtRS256.key');
+// const algorithm = 'RS256'
+// const pkcs8 = fs.readFileSync('./jwtRS256.key');
 // const rsaPrivateKey = await importPKCS8(pkcs8, algorithm)
 const rsaPrivateKey = async function() {
     await importPKCS8(pkcs8, algorithm)
@@ -33,25 +33,39 @@ app.get('/posts', authenticateToken, (req,res) => {
 })
 
 async function authenticateToken(req,res,next){
+    const secret = `MIIBPAIBAAJBALJaeVlgMfxZUWglk0PZZBkbmnkZkFAbqtaTJqeCIXa8xscPVvxJ
+    h2JjhwC4OwsVBgHvrhIwVLodgrQbYRpuVF0CAwEAAQJABIwoAe5g9+UzHSuwGI/H
+    bJh2lNXhBxndfkEcQDMiNUvJ22rVf+v+YsrxMLWkwoss3jERZ7JKNFepbNuir9SG
+    IQIhAP1406f4FoWMDwfjMjf1rkCEfyOzAZ320us8mZBPMFPFAiEAtCHaG14NlC1z
+    mV29jWVh9YoDECcoS2zAvunJRv6IT7kCIQD7Cgw2s9M6eTj5yt8V5VGrvI5fQQ88
+    8BR9vwsojgWDMQIhAKdAo1Yz3yHNjf9CBcVq9CjbS3rNEOHviYv6YNQVdBWpAiEA
+    mlJ9E20+boNRHie/PGmjZyOM9mkjOyLLMqIbGhRe4vM=`
+    var uint8array = new TextEncoder("utf-8").encode(secret); //convert secret to uint8array  
+    
    //token header
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
    //Bearer TOKEN sebagai nama token
     if (token == null) return res.sendStatus(401) //jika user tidak memiliki token maka akan return user dan status 401 untuk tidak memilki akses
-
+    const { payload, protectedHeader } = await jwtVerify(token, uint8array, {
+        issuer: 'urn:example:issuer',
+        audience: 'urn:example:audience'
+      })
+      console.log(protectedHeader)
+      console.log(payload)
     //verifikasi token
     // jwt.verify(token, publicKey, (err,user) => {
     //     if(err) return res.sendStatus(403) //invalid token
     //     req.user = user
     //     next()
-    // })
-    const { payload, protectedHeader } = await jwtVerify(token, rsaPrivateKey, {
-        issuer: 'urn:example:issuer',
-        audience: 'urn:example:audience'
-      })
+    // // })
+    // const { payload, protectedHeader } = await jwtVerify(token, rsaPrivateKey, {
+    //     issuer: 'urn:example:issuer',
+    //     audience: 'urn:example:audience'
+    //   })
       
-      console.log(protectedHeader)
-      console.log(payload)
+    //   console.log(protectedHeader)
+    //   console.log(payload)
 }
 
 app.listen(3000)
