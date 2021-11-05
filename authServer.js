30c7e61e09b011501fe6a8227090a21cb8ec3c9d
skyyudha <dmsleroux@gmail.com>
2021-11-05 11:23:54 +0700

    perbaikan dokumentasi + implementasi JWE
    masih ada error waktu generate compact JWE: Data must be a buffer.

--- authServer.js
+++ authServer.js
@@ -1,76 +1,24 @@
-//console.log("kamu")
 require('dotenv').config()
 
 const express = require('express')
 const app = express()
+app.use(express.json()) //pasang JWT ke Apps
 
+const {generateKeyPair, SignJWT,exportJWK, importJWK, generateSecret,CompactEncrypt } = require('jose') //ambil module2 dari lib jose
+
+var Rasha = require('rasha');//library untuk pem2jwk & jwk2pem
 const fs = require('fs')
 // var accessPrivateKey = fs.readFileSync('./jwtRS256.key');
 // var accessPublicKey = fs.readFileSync('./jwtRS256.key.pub');
 
-var refreshPrivateKey = fs.readFileSync('./refreshJwtRS256.key');
-var refreshPublicKey = fs.readFileSync('./refreshJwtRS256.key.pub');
+// var refreshPrivateKey = fs.readFileSync('./refreshJwtRS256.key');
+// var refreshPublicKey = fs.readFileSync('./refreshJwtRS256.key.pub');
 
-//const { SignJWT } = require('jose/jwt/sign') //get library JWT
-//const jose = require('jose');
 
-app.use(express.json()) //pasang JWT ke Apps
-//const { importPKCS8 } = require('jose')
-const {generateKeyPair, SignJWT,importSPKI,exportJWK,importPKCS8, importJWK, generateSecret  } = require('jose')
-
-//const { importJWK } = require('jose')
-// (async() => {
-//     console.log('before start');
-  
-//     const rsaPublicKey =  await importJWK({
-//         kty: 'RSA',
-//         e: 'AQAB',
-//         n: '12oBZRhCiZFJLcPg59LkZZ9mdhSMTKAQZYq32k_ti5SBB6jerkh-WzOMAO664r_qyLkqHUSp3u5SbXtseZEpN3XPWGKSxjsy-1JyEFTdLSYe6f9gfrmxkUF_7DTpq0gn6rntP05g2-wFW50YO7mosfdslfrTJYWHFhJALabAeYirYD7-9kqq9ebfFMF4sRRELbv9oi36As6Q9B3Qb5_C1rAzqfao_PCsf9EPsTZsVVVkA5qoIAr47lo1ipfiBPxUCCNSdvkmDTYgvvRm6ZoMjFbvOtgyts55fXKdMWv7I9HMD5HwE9uW839PWA514qhbcIsXEYSFMPMV6fnlsiZvQQ'
-//       }, 'PS256')
-    
-//     console.log('after start');
-//   })();
-
-  // const algorithm = 'ES256'
-  // const pkcs8 = fs.readFileSync('./jwtRS256.key.pub');
-  // const pkcs8 = `-----BEGIN RSA PUBLIC KEY-----
-  // MIIBCgKCAQEA24HWTGus/j/xaCnMBkABdozWw3Kph/g7yg2QcSHuHXQXUaDmNhYG
-  // hFgF1EOT6WOZrCqwTxUGbTRPibJ68VRWxGPnPxuWtfRqopzrAaPf41RqwzqIMN8Z
-  // lI6BY/ipxNESJRcM5PB2VF7PW0jHHd557+h10AUhiYj0UTjb+Mf37N7hxO0lD37n
-  // J588bVQk92ziDhf7AHBMc2n7O29E0YxoG9b3AUO/y9C7gagLCrIv0FPNf88Gcd4v
-  // BVEd8sryh/a+OwkaXlXQbEP/aL95EDbGG1ucDDK8ZDV3guEgbAd7RF6Tq9V3nDZ2
-  // e8mGBnp5ak8BejTr1VRkYqZA6gLO2vxhMQIDAQAB
-  // -----END RSA PUBLIC KEY-----`
-  // const pkcs8 = `-----BEGIN PRIVATE KEY-----
-  // MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgiyvo0X+VQ0yIrOaN
-  // nlrnUclopnvuuMfoc8HHly3505OhRANCAAQWUcdZ8uTSAsFuwtNy4KtsKqgeqYxg
-  // l6kwL5D4N3pEGYGIDjV69Sw0zAt43480WqJv7HCL0mQnyqFmSrxj8jMa
-  // -----END PRIVATE KEY-----`
-  // const ecPublicKey = await importSPKI(spki, algorithm)
-
-//   const algorithm = 'ES256'
-// const pkcs8 = `-----BEGIN PRIVATE KEY-----
-// MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgiyvo0X+VQ0yIrOaN
-// nlrnUclopnvuuMfoc8HHly3505OhRANCAAQWUcdZ8uTSAsFuwtNy4KtsKqgeqYxg
-// l6kwL5D4N3pEGYGIDjV69Sw0zAt43480WqJv7HCL0mQnyqFmSrxj8jMa
-// -----END PRIVATE KEY-----`
-// const ecPrivateKey = await importPKCS8(pkcs8, algorithm)
-// const rsaPublicKey = async function() {
-//     return await importPKCS8(pkcs8, algorithm)
-    
-//     //console.log(testkey)
-//   }
-//   console.log("hai")
-//   console.log(typeof rsaPublicKey); //function
-// const privateJwk = await exportJWK(privateKey)
-// const publicJwk = await exportJWK(publicKey)
-  
-// console.log(privateJwk)
-// console.log(publicJwk)
+//##//verifikasi dan generate refreshToken baru dari refreshToken lama
 
 // let refreshTokens = [] //not recomended in real production only for demonstrate
 //it will make refreshTokens empting out
-
 //###masih ada masalah dalam verify refresh token.
 //###refreshTokens array dianggap tidak berisi refreshToken. 
 // app.post('/token', (req,res) => {
@@ -92,26 +40,14 @@ app.delete('/logout',(req,res)=>{
     res.sendStatus(204)
 })
 
-
-// fs.writeFileSync("secret.key.pub", privateJwk);
-
-// const secret = async function() { await generateSecret('HS256') }
-// const privateJwk = async function() {  await exportJWK(secret) }
-//       console.log("batas A") 
-// console.log(secret)
-// console.log("typeof = " + typeof secret)    
-// console.log(privateJwk)
-// console.log("typeof = " + typeof privateJwk)
-//       console.log("batas A") 
-
 app.post('/login',async (req,res) => { //diganti jadi post karena mau ngirim token
 //bagian authorization
     const username = req.body.username //gk tau jelasinnya gimana
-    console.log("typeof = " + typeof username)
-    console.log("secret = " + username)
+    // console.log("typeof = " + typeof username)
+    // console.log("secret = " + username)
     const user = {name: username}
     
-    let { accessToken, publicJwk }  = await  generateAccessToken(user) //panggil sync fungsi
+    let { accessToken, publicJwk }  = await  generateAccessToken(user) // generate token JWT
     // console.log("batas B")
     // console.log(accessToken)
     // console.log(publicJwk)
@@ -120,39 +56,52 @@ app.post('/login',async (req,res) => { //diganti jadi post karena mau ngirim tok
     res.json({ accessToken: accessToken, secret: publicJwk }) //return value
 })
 
-var Rasha = require('rasha');
+
 
 async function generateAccessToken(user){
-  const { publicKey, privateKey } = await generateKeyPair('RS256')
-// console.log(publicKey)
-// console.log(privateKey)
-    // const privateKeyRaw = await exportJWK(privateKey)
-    const publicKeyRaw = await exportJWK(publicKey)
-    const pubPEM = await Rasha.export({ jwk: publicKeyRaw }).then(function (pem) {
-      // PEM in PKCS1 (traditional) format
-      console.log(pem);
-      return pem
-    });    
-    fs.writeFileSync("public11.pem", pubPEM);
-    // console.log("batas B")
-    //     console.log(pubPEM);
-    // console.log("batas B")
+    // const { publicKey, privateKey } = await generateKeyPair('RS256') //generate Key format objectKey
+    // console.log(publicKey)
+    // console.log(privateKey)
+      const {
+        privateKey,
+        publicKey
+    } = await generateKeyPair('PS256');
 
-    // Rasha.import({ pem: pubPEM }).then(function (jwk) {
-    //   console.log(jwk);
-    // });
+    const secret = await generateSecret('A256GCM')
+    const payload = {
+      "data": "xyz"
+  }
+    const privateKeyRaw = await exportJWK(privateKey)
+    const publicKeyRaw = await exportJWK(publicKey) //export key to JWK
+
+    await Rasha.export({ jwk: privateKeyRaw }).then(function (pem) { //convert jwk2 pem & export to file
+      // PEM in PKCS1 (traditional) format
+      // console.log("batas B")
+      // console.log(pem);
+      // console.log("batas B")
+      fs.writeFileSync("public11.pem", pem); //export file pem
+      // return pem
+    });    
 
     // var uint8array = new TextEncoder("utf-8").encode(secret); //convert secret to uint8array
-    const jwt = await new SignJWT({ 'urn:example:claim': true }) // generate token JWT // SignJWT dari library
-  .setProtectedHeader({ alg: 'RS256' })
-  .setIssuedAt()
-  .setIssuer('urn:example:issuer')
-  .setAudience('urn:example:audience')
-  .setExpirationTime('2h')
-  .sign(privateKey)
+    const uint8array = new TextEncoder("utf-8").encode(JSON.stringify(payload));
+    const jwe = await new CompactEncrypt(uint8array)
+        .setProtectedHeader({
+            alg: 'RSA-OAEP-256',
+            enc: 'A256GCM'
+        })
+        .setContentEncryptionKey(secret)
+        .encrypt(privateKey)
 
-  let accessToken = jwt, publicJwk = publicKeyRaw;
-  return { accessToken, publicJwk};  
+  //   const jwt = await new SignJWT({ 'urn:example:claim': true }) // generate token JWT // SignJWT dari library
+  // .setProtectedHeader({ alg: 'RS256' })
+  // .setIssuedAt()
+  // .setIssuer('urn:example:issuer')
+  // .setAudience('urn:example:audience')
+  // .setExpirationTime('2h')
+  // .sign(privateKey) //untuk key yg digunakan adalah key yg formatnya objectKey
+
+  let accessToken = jwe, publicJwk = privateKeyRaw; return { accessToken, publicJwk};  //set variable & return value
 }
   
 app.listen(4000)
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-//console.log("kamu")
 require('dotenv').config()
-var Rasha = require('rasha');
+
 
 const express = require('express')
 const app = express()
+var Rasha = require('rasha'); //library untuk pem2jwk & jwk2pem
 
 const fs = require('fs')
-var publicKey = fs.readFileSync('./public11.pem', 'utf8');
+var publicKey = fs.readFileSync('./public11.pem', 'utf8'); //ambil key dari filem pem
+//test print 
+// console.log("batas B"); console.log(publicKey); console.log("type= "+typeof(publicKey)); console.log("batas B/");
+
 // var publicKey = fs.readFileSync('./secret.key.pub', 'utf8');
 
-// const jwt = require("jsonwebtoken") //get library JWT
 const { jwtVerify, importJWK  } = require('jose')
 app.use(express.json()) //pasang JWT ke Apps
-// const algorithm = 'RS256'
-// const pkcs8 = fs.readFileSync('./jwtRS256.key');
-// const rsaPrivateKey = await importPKCS8(pkcs8, algorithm)
-// const rsaPrivateKey = async function() {
-//     await importPKCS8(pkcs8, algorithm)
-//     //console.log(testkey)
-//   }
+
 const posts = [
     {
         username: 'lala',
@@ -35,64 +31,31 @@ app.get('/posts', authenticateToken, (req,res) => {
 })
 
 async function authenticateToken(req,res,next){
-    // const secret = `MIIBPAIBAAJBALJaeVlgMfxZUWglk0PZZBkbmnkZkFAbqtaTJqeCIXa8xscPVvxJ
-    // h2JjhwC4OwsVBgHvrhIwVLodgrQbYRpuVF0CAwEAAQJABIwoAe5g9+UzHSuwGI/H
-    // bJh2lNXhBxndfkEcQDMiNUvJ22rVf+v+YsrxMLWkwoss3jERZ7JKNFepbNuir9SG
-    // IQIhAP1406f4FoWMDwfjMjf1rkCEfyOzAZ320us8mZBPMFPFAiEAtCHaG14NlC1z
-    // mV29jWVh9YoDECcoS2zAvunJRv6IT7kCIQD7Cgw2s9M6eTj5yt8V5VGrvI5fQQ88
-    // 8BR9vwsojgWDMQIhAKdAo1Yz3yHNjf9CBcVq9CjbS3rNEOHviYv6YNQVdBWpAiEA
-    // mlJ9E20+boNRHie/PGmjZyOM9mkjOyLLMqIbGhRe4vM=`
-    // const refreshToken = req.body.secret
     // var uint8array = new TextEncoder("utf-8").encode(secret); //convert secret to uint8array    
-   //token header
-   console.log("batas B");
-   console.log(publicKey);
-   console.log("type= "+typeof(publicKey));
-   console.log("batas B/");
 
-    const publicKeyRaw = await Rasha.import({ pem: publicKey }).then(function (jwk) {
+    const publicKeyRaw = await Rasha.import({ pem: publicKey }).then(function (jwk) { //convert pem2jwk
         console.log(jwk);
         return jwk;
-    });
-    console.log("batas A");
-    console.log(publicKeyRaw);
-    console.log("type= "+typeof(publicKeyRaw));
-    console.log("batas A/");
+    });     
+    // console.log("batas A"); console.log(publicKeyRaw);  console.log("type= "+typeof(publicKeyRaw));  console.log("batas A/"); //test print 
 
-    const RSAPublicKey = await importJWK(publicKeyRaw, 'RS256')
-    console.log("batas C");
-    console.log(RSAPublicKey);
-    console.log("type= "+typeof(publicRSAPublicKeyKeyRaw));
-    console.log("batas C/");
+    const RSAPublicKey = await importJWK(publicKeyRaw, 'RS256') //import jwk to objectKey    
+    // console.log("batas C"); console.log(RSAPublicKey); console.log("type= "+typeof(publicRSAPublicKeyKeyRaw)); console.log("batas C/"); //test print 
  
-   const authHeader = req.headers['authorization']
+    const authHeader = req.headers['authorization'] //ambil html header authorization
     const token = authHeader && authHeader.split(' ')[1]
-    // const secret = req.body.secret
-    // console.log("typeof = " + typeof secret)
-    // console.log("secret = " + secret)
-    // const rsaPublicKey = await importJWK(secret)
-    // console.log("key= " + rsaPublicKey)
+
    //Bearer TOKEN sebagai nama token
+
     if (token == null) return res.sendStatus(401) //jika user tidak memiliki token maka akan return user dan status 401 untuk tidak memilki akses
+    //verifikasi token
     const { payload, protectedHeader } = await jwtVerify(token, RSAPublicKey, {
         issuer: 'urn:example:issuer',
         audience: 'urn:example:audience'
       })
+
       console.log(protectedHeader)
       console.log(payload)
-    //verifikasi token
-    // jwt.verify(token, publicKey, (err,user) => {
-    //     if(err) return res.sendStatus(403) //invalid token
-    //     req.user = user
-    //     next()
-    // // })
-    // const { payload, protectedHeader } = await jwtVerify(token, rsaPrivateKey, {
-    //     issuer: 'urn:example:issuer',
-    //     audience: 'urn:example:audience'
-    //   })
-      
-    //   console.log(protectedHeader)
-    //   console.log(payload)
 }
 
 app.listen(3000)
