bfb0c78cd8b2c32c6775281936b9462bbddd4967
skyyudha <dmsleroux@gmail.com>
2021-12-29 22:54:58 +0700

    ganti sikit readme

--- Authserver.js
+++ Authserver.js
@@ -1,60 +1,89 @@
-//console.log("kamu")
 require('dotenv').config()
+
 
 const express = require('express')
 const app = express()
+// var Rasha = require('rasha');//library untuk pem2jwk & jwk2pem KHUSUS RSA
+// var Eckles = require('eckles'); //library untuk pem2jwk & jwk2pem KHUSUS EC
 
 const fs = require('fs')
-var accessPrivateKey = fs.readFileSync('./private11.pem');
-var accessPublicKey = fs.readFileSync('./public11.pem');
+var publicKeyRaw = fs.readFileSync('./public11.pem', 'utf8'); //ambil key dari filem pem
+var privateKeyRaw = fs.readFileSync('./private11.pem', 'utf8'); //ambil key dari filem pem
 
-const jwt = require("jsonwebtoken") //get library JWT
+//test print 
+// console.log("batas B"); console.log(publicKey); console.log("type= "+typeof(publicKey)); console.log("batas B/");
 
+// var publicKey = fs.readFileSync('./secret.key.pub', 'utf8');
+
+const { jwtVerify, importJWK, compactDecrypt,importPKCS8,
+    generateKeyPair,   exportPKCS8, exportSPKI, importSPKI, exportJWK  } = require('jose')
 app.use(express.json()) //pasang JWT ke Apps
 
-let refreshTokens = [] //not recomended in real production only for demonstrate
-//it will make refreshTokens empting out
+const posts = [
+    {
+        username: 'lala',
+        title: 'Post 1'
+    },
+    {
+        username: 'lili',
+        title: 'Post 2'
+    }
+]
 
-//###masih ada masalah dalam verify refresh token.
-//###refreshTokens array dianggap tidak berisi refreshToken. 
-app.post('/token', (req,res) => {
-    const refreshToken = req.body.token
-    if (refreshToken == null) return res.sendStatus(401)
-    //console.log(refreshTokens)
-    // if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, refreshPublicKey,(err,user) =>{ //verifikasi apakah refresh token valid
-        //jika refresh token valid maka akan langsung generate access token baru.
-        //kenapa access token yang lama tidak divalidasi ?
-        // if(err) return res.sendStatus(403) //###masih kena error. tapi yang ini gk tau alasannya
-        const accessToken = generateAccessToken({name: user.name}) //generate access token baru
-        res.json({accessToken: accessToken})
-    })
+//sekarang coba bikin middleware untuk /posts
+app.get('/posts', authenticateToken, (req,res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+
 })
 
-app.delete('/logout',(req,res)=>{
-    refreshTokens = refreshTokens.filter(token =>token !== req.body.token)
-    res.sendStatus(204)
+app.post('/getKey', async (req,res) => { //generate key pair + export public, private
+    const algoritmaKey = 'ECDH-ES'
+
+    const {
+        privateKey,
+        publicKey
+    } = await generateKeyPair(algoritmaKey); 
+
+    //export Object key jadi PEM (PKCS8 format)
+    const privateKeyRaw = await exportPKCS8(privateKey);
+    console.log(await exportJWK(privateKey));
+    console.log(privateKeyRaw);
+    fs.writeFileSync("private11.pem", privateKeyRaw); //export file pem
+
+    //export Object key jadi PEM (SPKI format)
+    const publicKeyRaw = await exportSPKI(publicKey);
+    console.log(await exportJWK(publicKey));
+    console.log(publicKeyRaw);
+    fs.writeFileSync("public11.pem", publicKeyRaw); //export file pem
+
+    // res.sendStatus(200)
+    res.json({ status: "Generate Key Pair Sukses Bro"})
 })
 
-app.post('/login',(req,res) => { //diganti jadi post karena mau ngirim token
-//bagian untuk Autthenticate user 
-    //tapi diskip dulu karena mau fokus ke Authorization
+async function authenticateToken(req,res,next){
+    const algoritmaKey = 'ECDH-ES'
+    const secretKey = await importPKCS8(privateKeyRaw, algoritmaKey) //import jwk to objectKey    
+ 
+    const authHeader = req.headers['authorization'] //ambil html header authorization
+    const token = authHeader && authHeader.split(' ')[1] //ambil hanya tokennya saja
+    //Bearer TOKEN sebagai jenis token
+    
+    //verifikasi token
+    if (token == null) return res.sendStatus(401) //jika user tidak memiliki token maka akan return user dan status 401 untuk tidak memilki akses
+    
+    const decoder = new TextDecoder()
+    const {
+        plaintext,
+        protectedHeader
+        // , iv, ciphertext, tag //tidak dikeluarkan default di lib-nya. //alasan keamanan
+    } = await compactDecrypt(token, secretKey)
+    // const content = 
 
-//bagian authorization
-    const username = req.body.username //gk tau jelasinnya gimana
-    const user = {name: username}
-    
-    const accessToken =  generateAccessToken(user)
-    const refreshToken =  jwt.sign(user,refreshPrivateKey,{ algorithm: 'RS256', expiresIn: '5m'})
-    //refreshTokens.push(refreshToken) //send refreshToken to refreshTokens Array
-    res.json({ accessToken: accessToken, refreshToken: refreshToken}) //return value
-})
-
-function generateAccessToken(user){
-    
-    // jwt.sign( user, process.env.ACCESS_TOKEN_SECRET) //ambil payload dari jwt header
-    return jwt.sign(user, accessPrivateKey, { algorithm: 'RS256', expiresIn: '5m' }) //ambil payload dari jwt header
-    
+    console.log(protectedHeader)
+    // console.log(decoder.decode(iv))
+    // console.log(decoder.decode(ciphertext))
+    // console.log(plaintext)
+    console.log(decoder.decode(plaintext))
 }
 
-app.listen(5000)
+app.listen(3000)
--- README.md
+++ README.md
@@ -1,32 +1,29 @@
-# JWT-node.js-RSA
-# JWT-node.js-RSA
+# JWE-node.js-RSA
+# JWE-node.js-RSA
 
 ### Desc
-Hanya digunakan untuk simulasi pertukaran token (JWT) pada rest API
+note:"masih dalam proses revisi"
 
 ### Requirement:
 1. Rest client Extensions vscode
 2. npm
 3. nodemon
-4. jsonwebtoken || npm install jsonwebtoken || https://github.com/auth0/node-jsonwebtoken
+4. JOSE || npm install JOSE || https://github.com/panva/jose
 
 ### Ada 2 server:
-1. authServer.js (Generate access token dan refresh token)
-2. server.js (client)
+1. authServer.js (Generate key )
+2. server1.js (client)
+3. server2.js (client)
 
 ### Alur
 Gunakan request.rest untuk testing
 
-login -> copy access token ke Auth /posts -> copy refresh token ke "token" /logout dan ke "token" /token -> request get /posts -> jika username sama = sukses
+note:"masih dalam proses revisi"
 
 ### pair key yang digunakan:
-1. jwtRS512.key
-2. jwtRS512.key.pub
-3. refreshJwtRS512.key
-4. refreshJwtRS512.key.pub
+note:"masih dalam proses revisi"
 
 ### NOTE:
 ##### Semua petunjuk tambahan ada di file: guide newbie nodeJS.txt
-#### file yang memiliki nama file part adalah bentuk dokumentasi langkah-langkah menuju final //lupa bikin branchnya hehehe
   
 # JWT-node.js-RSA
